7799bd09b93d48ff7c02a12bd7bfbc5ecce54e97
dezlorator <danilprohoren@gmail.com>
2020-07-03 10:30:07 +0300

    Add project files.

---
+++ TestingConnectionWithPostgre/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestingConnectionWithPostgre.Models;
+
+namespace TestingConnectionWithPostgre.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly MyWebApiContext _context;
+
+        public PersonController(MyWebApiContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreatePerson()
+        {
+            var person = new Person { Address = "hoho", City = "Dnipro", FirstName = "Vasyan", LastName = "Pupkin" };
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ TestingConnectionWithPostgre/Migrations/20200702142600_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TestingConnectionWithPostgre.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LastName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
